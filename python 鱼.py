10f9afd0474bef4e840be9142eb2e695a083afda
117010800101 <43258784+117010800101@users.noreply.github.com>
2018-09-20 19:37:03 +0800

    Add files via upload

---
+++ python 鱼.py
@@ -0,0 +1,26 @@
+from turtle import*
+screensize(650,360,"blue")
+penup()
+goto(0,0)
+pendown()
+pensize(2)
+seth(30)
+fd(120)
+seth(-90)
+fd(120)
+seth(-210)
+fd(120)
+penup()
+goto(103,0)
+pendown()
+goto(123,20)
+goto(123,-20)
+goto(103,0)
+penup()
+goto(20,0)
+pendown
+circle(10)
+
+                                                  
+
+
